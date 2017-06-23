598670447088e32b80d14d905d3c44ad2d8639ff
adam-toth-n <adam.toth.n@gmail.com>
2017-06-23 18:02:35 +0100

    added some comments

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -1,29 +1,26 @@
 $(document).ready(function(){
-  
+
   function getWeather(position) {
-    
     var currentPos = position.coords;
     document.getElementById("latitude").innerHTML = "<p>" + (`Latitude : ${currentPos.latitude}`) + "</p>";
     document.getElementById("longitude").innerHTML = "<p>" + (`Longitude: ${currentPos.longitude}`) + "</p>";
-    document.getElementById("moreOrLess").innerHTML = "<p>" + (`More or less ${currentPos.accuracy} meters.`) + "</p>";  
-    
+    document.getElementById("moreOrLess").innerHTML = "<p>" + (`More or less ${currentPos.accuracy} meters.`) + "</p>";
+
     $.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyC9OxmAbzqqnkNGex-Q_fA0uLgFq1KhhW4',function(data) {
     lat = currentPos.latitude;
     lon = currentPos.longitude;
-    console.log(data);
+    console.log(data); //returns the object to work with
     })
-  }; 
-  
-  
+  };
+
+  //log eventual errors
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   };
-  
-  navigator.geolocation.getCurrentPosition(getWeather, error); 
-  
+
+  navigator.geolocation.getCurrentPosition(getWeather, error);
+
   $.get("https://ipinfo.io", function(response) {
       document.getElementById("locality").innerHTML = "<p>" + (response.city) + "</p>";
   }, "jsonp");
-});  
-
-
+});
